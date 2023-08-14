db63405fd77d54a2debc1c27d18d39e995ad2e4d
Abhishek Ravula <ravulaabhishek@gmail.com>
2023-08-14 13:48:39 +0530

    added loader for all playlists

--- src/spotify/components/AllPlaylists.js
+++ src/spotify/components/AllPlaylists.js
@@ -7,10 +7,13 @@ import { HOSTNAME } from '../spotify.constants.js'
 function AllPlaylists() {
 
     const [AllPlaylistData, setAllPlaylistData] = useState([])
+    const [loading, setLoading] = useState(false)
     const token = localStorage.getItem('token')
 
 
+
     useEffect(() => {
+        setLoading(true)
         fetch(HOSTNAME + "playlists/", {
             "headers": {
                 "Authorization": `Token ${token}`
@@ -24,12 +27,15 @@ function AllPlaylists() {
             })
             .catch(err => {
                 console.error(err);
+            }).finally(() => {
+                setLoading(false)
             });
     }, [])
 
 
     return (
         <Grid>
+            {loading && <h3>Loading...</h3>}
             {!!AllPlaylistData?.length && AllPlaylistData.map(playlist => {
                 return <Playlist key={playlist.id} {...playlist} />
             })}
